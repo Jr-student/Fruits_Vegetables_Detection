41920b05a7398679c867cd1ea7c764b3361cbced
Ali <91473044+Jr-student@users.noreply.github.com>
2024-01-25 14:12:02 +0300

    Update detection.py

--- Fruits_Vegetables_Detection/detection.py
+++ Fruits_Vegetables_Detection/detection.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 import cv2
 
-model = YOLO("C:/Users/user/Desktop/Food_detection2/Meyve_Sebze.pt")
+model = YOLO("/path/to/best.pt")
 model.predict(source= "0", show=True, conf=0.7)
